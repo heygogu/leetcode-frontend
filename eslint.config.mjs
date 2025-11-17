12a08b04b9b6b3bda35c430fd36eb79aab355e90
Rohit <rohitnarnolia88@gmail.com>
2025-11-17 21:00:13 +0530

    change es lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,9 +19,11 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
-    "rules": {
-    "@typescript-eslint/no-explicit-any": "off"
-    }
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
   },
 ];
 
